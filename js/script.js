1e72d7ca1e90461cf36a401877c49e569445ec0d
Emily Chao <elchao96@live.com>
2016-04-11 15:58:48 -0500

    starting foundation for sentiment

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function getDuration(minutes, seconds) {
     return minutes + (seconds / 60);
 }
 
-function transformData(data) {
+function transformTalkingTimesData(data) {
     var transformed = [];
     var currentCandidates = ["Donald Trump", "John Kasich", "Ted Cruz", "Hillary Clinton", "Bernie Sanders"];
     for (var i = 0; i < data.length; i++) {
@@ -22,128 +22,56 @@ function transformData(data) {
     return transformed;
 }
 
-function drawTalkingTimesRep() {
-    var svg = dimple.newSvg('#talkingTime', 1200, 500);
-    d3.json('data/talkingtimes-rep.json', function(result) {
-        var data = transformData(result.data);
+function drawTalkingTimes(jsonfile) {
+    var svg = dimple.newSvg('#talkingTime', 1000, 500);
+    d3.json(jsonfile, function(result) {
+        var data = transformTalkingTimesData(result.data);
         var chart = new dimple.chart(svg, data);
         chart.addCategoryAxis('x', ['date', 'candidate', 'moderator']);
         chart.addMeasureAxis('y', 'talkingTime');
         var candidateSeries = chart.addSeries('candidate', dimple.plot.bar);
         var moderatorSeries = chart.addSeries('moderator');
-        var candidateLegend = chart.addLegend(65, 10, 510, 50, 'right', candidateSeries);
-        var moderatorLegend = chart.addLegend(600, 10, 510, 50, 'right', moderatorSeries);
+        var candidateLegend = chart.addLegend(100, 10, 300, 50, 'right', candidateSeries);
+        var moderatorLegend = chart.addLegend(500, 10, 300, 50, 'right', moderatorSeries);
         chart.draw();
 
-        //var candidateFilters = dimple.getUniqueValues(data, 'candidate');
         var moderatorFilters = dimple.getUniqueValues(data, 'moderator');
         chart.legends = [candidateLegend];
-        /*candidateLegend.shapes.selectAll('rect')
-            .on('click', function (e) {
-                var hide = false;
-                var newFilters = [];
-                candidateFilters.forEach(function (f) {
-                    if (f === e.aggField.slice(-1)[0]) {
-                        hide = true;
-                    } else {
-                        newFilters.push(f);
-                    }
-                });
-                if (hide) {
-                    d3.select(this).style('opacity', 0.2);
-                } else {
-                    newFilters.push(e.aggField.slice(-1)[0]);
-                    d3.select(this).style('opacity', 0.8);
-                }
-                candidateFilters = newFilters;
-                chart.data = dimple.filterData(data, 'candidate', candidateFilters);
-                chart.draw(800);
-            });*/
         moderatorLegend.shapes.selectAll('rect')
-            .on('click', function(e) {
-                var newFilters = [];
-                var hide = false;
-                moderatorFilters.forEach(function (f) {
-                    if (f === e.aggField.slice(-1)[0]) {
-                        hide = true;
-                    } else {
-                        newFilters.push(f);
-                    }
-                });
-                if (hide) {
-                    d3.select(this).style('opacity', 0.2);
+        .on('click', function(e) {
+            var newFilters = [];
+            var hide = false;
+            moderatorFilters.forEach(function (f) {
+                if (f === e.aggField.slice(-1)[0]) {
+                    hide = true;
                 } else {
-                    newFilters.push(e.aggField.slice(-1)[0]);
-                    d3.select(this).style('opacity', 0.8);
+                    newFilters.push(f);
                 }
-                moderatorFilters = newFilters;
-                chart.data = dimple.filterData(data, 'moderator', moderatorFilters);
-                chart.draw(800);
             });
+            if (hide) {
+                d3.select(this).style('opacity', 0.2);
+            } else {
+                newFilters.push(e.aggField.slice(-1)[0]);
+                d3.select(this).style('opacity', 0.8);
+            }
+            moderatorFilters = newFilters;
+            chart.data = dimple.filterData(data, 'moderator', moderatorFilters);
+            chart.draw(800);
+        });
     });
 }
 
-drawTalkingTimesRep();
+function transformTopicsData(data) {
 
-function drawTalkingTimesDem() {
-    var svg = dimple.newSvg('#talkingTime', 1200, 500);
-    d3.json('data/talkingtimes-dem.json', function(result) {
-        var data = transformData(result.data);
-        var chart = new dimple.chart(svg, data);
-        chart.addCategoryAxis('x', ['date', 'candidate', 'moderator']);
-        chart.addMeasureAxis('y', 'talkingTime');
-        var candidateSeries = chart.addSeries('candidate', dimple.plot.bar);
-        var moderatorSeries = chart.addSeries('moderator');
-        var candidateLegend = chart.addLegend(65, 10, 510, 50, 'right', candidateSeries);
-        var moderatorLegend = chart.addLegend(600, 10, 510, 50, 'right', moderatorSeries);
-        chart.draw();
-
-        //var candidateFilters = dimple.getUniqueValues(data, 'candidate');
-        var moderatorFilters = dimple.getUniqueValues(data, 'moderator');
-        chart.legends = [candidateLegend];
-        /*candidateLegend.shapes.selectAll('rect')
-            .on('click', function (e) {
-                var hide = false;
-                var newFilters = [];
-                candidateFilters.forEach(function (f) {
-                    if (f === e.aggField.slice(-1)[0]) {
-                        hide = true;
-                    } else {
-                        newFilters.push(f);
-                    }
-                });
-                if (hide) {
-                    d3.select(this).style('opacity', 0.2);
-                } else {
-                    newFilters.push(e.aggField.slice(-1)[0]);
-                    d3.select(this).style('opacity', 0.8);
-                }
-                candidateFilters = newFilters;
-                chart.data = dimple.filterData(data, 'candidate', candidateFilters);
-                chart.draw(800);
-            });*/
-        moderatorLegend.shapes.selectAll('rect')
-            .on('click', function(e) {
-                var newFilters = [];
-                var hide = false;
-                moderatorFilters.forEach(function (f) {
-                    if (f === e.aggField.slice(-1)[0]) {
-                        hide = true;
-                    } else {
-                        newFilters.push(f);
-                    }
-                });
-                if (hide) {
-                    d3.select(this).style('opacity', 0.2);
-                } else {
-                    newFilters.push(e.aggField.slice(-1)[0]);
-                    d3.select(this).style('opacity', 0.8);
-                }
-                moderatorFilters = newFilters;
-                chart.data = dimple.filterData(data, 'moderator', moderatorFilters);
-                chart.draw(800);
-            });
-    });
 }
 
-drawTalkingTimesDem();
+function drawSentimentAnalysis(jsonfile) {
+
+}
+
+$(document).ready(function() {
+    drawTalkingTimes('data/talkingtimes-rep.json');
+    drawTalkingTimes('data/talkingtimes-dem.json');
+    drawSentimentAnalysis('data/sentiments/rep_overall.json');
+    drawSentimentAnalysis('data/sentiments/dem_overall.json');
+});
