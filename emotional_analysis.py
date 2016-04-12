c123b4011896991ad38383d159355f9a15abe149
Meera Parat <meera.parat@gmail.com>
2016-04-12 02:13:36 -0500

    targeted sentiment analysis

--- emotional_analysis.py
+++ emotional_analysis.py
@@ -2,8 +2,8 @@ from alchemyapi import AlchemyAPI
 import json
 import sys
 import os
-
-#transcript = sys.argv[1]
+import csv
+from os.path import basename
 
 def getnames():
     with open("transcripts/Democrats/dem-2-4-2016.txt", 'r') as t:
@@ -32,22 +32,35 @@ def gettexts(names):
 def do_analysis(names, texts):
     alchemyapi = AlchemyAPI()
 
-    path = 'transcripts/Democrats/dem-2-4-2016.txt'
-    file_name = str(os.path.splitext(path)[0]) + '_sentiment.txt'
+    base=os.path.basename('transcripts/Democrats/dem-2-4-2016.txt')
+    debate_name = os.path.splitext(base)[0]
+
+    file_name = 'data/targeted sentiments/Democrat/' + str(debate_name) + '_sentiment.txt'
     f = open(file_name, 'w')
 
+    filepath = "data/topics/Democratic Debates_Top Three Topics.csv"
+    all_topics = csv.DictReader(open(filepath, "r"))
+    dt_1 = []
+
+    for row in all_topics:
+        dt_1.append(row[debate_name])
+    dt_2 = [x.split('\'') for x in dt_1]
+    dt_3 = [y  for x in dt_2[0] for y in x.split(',')]
+    
     for i in range(len(names)):
-        response = alchemyapi.sentiment_targeted('text', texts[i], 'healthcare')
+        for j in range(3):
+            topic = [y  for x in dt_2[j] for y in x.split(',')]
+            response = alchemyapi.sentiment_targeted('text', texts[i], topic[:(len(topic)-1)])
+            if response['status'] == 'OK':
+                f.write(topic[len(topic)-1] + ':\n')
+                f.write(names[i] + ' Sentiment \n')
+                f.write('type: ' + str(response['docSentiment']['type']) + '\n')
 
-        if response['status'] == 'OK':
-            f.write(names[i] + ' Sentiment \n')
-            f.write('type: ' + str(response['docSentiment']['type']) + '\n')
-
-            if 'score' in response['docSentiment']:
-                f.write('score: ' + str(response['docSentiment']['score']) + '\n \n')
-        else:
-            print('Error in sentiment analysis call: ',
-                  response['statusInfo'])
+                if 'score' in response['docSentiment']:
+                    f.write('score: ' + str(response['docSentiment']['score']) + '\n \n')
+            else:
+                print('Error in sentiment analysis call: ',
+                      response['statusInfo'])
 
 names = getnames()
 texts = gettexts(names)
